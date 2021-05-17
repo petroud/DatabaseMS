50ec9b198ffe1ef15dbe69a2923a74274782235a
Dimitris Petrou <47597523+petroud@users.noreply.github.com>
2021-05-17 16:29:21 +0300

    Phase A besides 6.2 completed

--- 4/calculate_completeness_4_5.sql
+++ 4/calculate_completeness_4_5.sql
@@ -1,13 +1,18 @@
-CREATE FUNCTION calculate_completeness_4_5(hotelid integer,yearArg integer) RETURNS TABLE("Month" text, "Completeness Percentage" bigint)
+CREATE OR REPLACE FUNCTION calculate_completeness_4_5(hotelid integer,yearArg integer) RETURNS TABLE("Month" text, "Completeness Percentage" double precision)
 AS 
 $$
 DECLARE
 	num_of_rooms integer;
+	monthno double precision;
 BEGIN
 	num_of_rooms = COUNT(*) FROM "room" WHERE "idHotel"=hotelid;
 
 	RETURN QUERY
-	SELECT CONCAT((TO_CHAR(TO_DATE((select extract(month from "checkin"))::text, 'MM'), 'Month')),' ',yearArg) AS "Month" , count(DISTINCT(roomid))*100/num_of_rooms AS "Completeness Percentage" FROM find_roombookings_of_hotel(hotelid) 
+	SELECT CONCAT((TO_CHAR(TO_DATE((select extract(month from "checkin"))::text, 'MM'), 'Month')),yearArg) AS "Month", 
+	
+		SUM(checkout-checkin)*100/(DATE_PART('days',DATE_TRUNC('month',CAST(CONCAT('2021-',LPAD((select EXTRACT(MONTH FROM "checkin"))::text,2,'0'),'-05') AS date)) + '1 MONTH'::INTERVAL - '1 DAY'::INTERVAL)*num_of_rooms) AS "Completeness Percentage" 
+	
+	FROM find_roombookings_of_hotel(hotelid) 
 	WHERE ((SELECT EXTRACT(YEAR FROM "checkin")) = yearArg)
 	GROUP BY (SELECT EXTRACT(MONTH FROM "checkin"));
 	
